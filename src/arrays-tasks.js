a6655cf0a52e30947af5286504c834614112aa66
Ilya Spirkin <spirkinilya@gmail.com>
2024-02-08 22:31:48 +0300

    feat: solve getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -132,8 +132,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length !== 0) {
+    return +(arr.reduce((sum, value) => sum + value, 0) / arr.length).toFixed(
+      2
+    );
+  }
+  return 0;
 }
 
 /**
