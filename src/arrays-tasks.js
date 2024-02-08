dee57f877a5dd10750c1b5d1764af5d11040b07e
Ilya Spirkin <spirkinilya@gmail.com>
2024-02-08 20:59:44 +0300

    feat: solve findElement

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,16 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let sumArr = [];
+  if (arr1.length < arr2.length) {
+    sumArr = arr1.map((value, index) => value + arr2[index]);
+    sumArr = sumArr.concat(arr2.slice(arr1.length));
+  } else {
+    sumArr = arr2.map((value, index) => value + arr1[index]);
+    sumArr = sumArr.concat(arr1.slice(arr2.length));
+  }
+  return sumArr;
 }
 
 /**
@@ -56,8 +64,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
