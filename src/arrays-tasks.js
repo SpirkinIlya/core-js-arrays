03951288f52a525a822ddb97232766490bf0b94e
Ilya Spirkin <spirkinilya@gmail.com>
2024-02-08 23:01:53 +0300

    feat: solve getTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -211,8 +211,11 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  if (n) {
+    return arr.slice(0 - n);
+  }
+  return [];
 }
 
 /**
