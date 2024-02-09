c5f48abdeb2b7d4c5eea0950a98149d76f29b4f7
Ilya Spirkin <spirkinilya@gmail.com>
2024-02-09 21:09:03 +0300

    feat: solve createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -351,8 +351,13 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((chunk, value, index) => {
+    if (index % chunkSize === 0) {
+      chunk.push(arr.slice(index, index + chunkSize));
+    }
+    return chunk;
+  }, []);
 }
 
 /**
